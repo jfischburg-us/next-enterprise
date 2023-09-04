146c3166ee7b8f92da5df21869d0cb21c41b87cc
Joshua Fischburg <jfischburg@lifemesh.ai>
2023-09-04 15:09:04 -0400

    Increase the font size of the "trAIdmark" text in the SupernovaLogo component.

--- components/SupernovaLogo/SupernovaLogo.tsx
+++ components/SupernovaLogo/SupernovaLogo.tsx
@@ -10,7 +10,7 @@ const SupernovaLogo = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
       <svg ref={logoRef} className="logo z-10" viewBox="0 -175 200 400">
-        <text fill="white" className="font-inter" textAnchor="middle" dominantBaseline="middle" style={{fontSize: 'calc(3vw + 1em)'}}>trAIdmark</text>
+        <text fill="white" className="font-inter" textAnchor="middle" dominantBaseline="middle" style={{fontSize: 'calc(5vw + 1em)'}}>trAIdmark</text>
         <text fill="white" className="font-inter" textAnchor="middle" dominantBaseline="middle" dy="2em" style={{fontSize: 'calc(1vw + 0.5em)'}}>An AI Value Creation Agency</text>
       </svg>
     </div>
