b01428a0b1f421b24e60cad53e2a362934e58228
Joshua Fischburg <jfischburg@lifemesh.ai>
2023-09-04 17:34:23 -0400

    aider: Added pages for services, AI consulting, and guru sessions.
    # Aider chat conversation:
    
    USER: Next, let's add some pages. Let's take care of the services group, so we will require services, ai consulting and guru sessions for 3 total pages.
    
    ASSISTANT: To add new pages for services, AI consulting, and guru sessions, we need to create new files in the `pages` directory. Each file will correspond to a route in the application.
    
    Here are the changes needed:
    
    ```tsx
    pages/services.tsx
    <<<<<<< HEAD
    =======
    import React from 'react';
    
    export default function Services() {
      return (
        <div>
          <h1>Services</h1>
          <p>Information about our services...</p>
        </div>
      );
    }
    >>>>>>> updated
    ```
    
    ```tsx
    pages/ai-consulting.tsx
    <<<<<<< HEAD
    =======
    import React from 'react';
    
    export default function AIConsulting() {
      return (
        <div>
          <h1>AI Consulting</h1>
          <p>Information about our AI consulting services...</p>
        </div>
      );
    }
    >>>>>>> updated
    ```
    
    ```tsx
    pages/guru-sessions.tsx
    <<<<<<< HEAD
    =======
    import React from 'react';
    
    export default function GuruSessions() {
      return (
        <div>
          <h1>Guru Sessions</h1>
          <p>Information about our guru sessions...</p>
        </div>
      );
    }
    >>>>>>> updated
    ```
    
    These new files will create the routes `/services`, `/ai-consulting`, and `/guru-sessions` respectively. You can replace the placeholder text with the actual content for each page.

---
+++ pages/ai-consulting.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AIConsulting() {
+  return (
+    <div>
+      <h1>AI Consulting</h1>
+      <p>Information about our AI consulting services...</p>
+    </div>
+  );
+}
---
+++ pages/guru-sessions.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function GuruSessions() {
+  return (
+    <div>
+      <h1>Guru Sessions</h1>
+      <p>Information about our guru sessions...</p>
+    </div>
+  );
+}
