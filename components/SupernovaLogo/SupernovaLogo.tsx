40a7c9ace112a8aa1989db31c1299735b74aff0c
Joshua Fischburg <jfischburg@lifemesh.ai>
2023-09-04 15:09:05 -0400

    aider: Center the supernova logo on the x-axis using CSS Flexbox.
    # Aider chat conversation:
    
    ASSISTANT: To fix the x-axis centering for the supernova logo, we need to modify the `SupernovaLogo.tsx` file. We can use CSS Flexbox to center the SVG both vertically and horizontally within its container.
    
    Here are the steps to make the necessary changes:
    
    1. Modify the `SupernovaLogo.tsx` file to apply CSS Flexbox to the SVG container.
    
    Here is the *edit block* for the change:
    
    ```tsx
    components/SupernovaLogo/SupernovaLogo.tsx
    <<<<<<< HEAD
        <div className="flex items-center justify-center w-full h-full">
          <svg ref={logoRef} className="logo z-10" viewBox="0 -175 200 400">
            <text fill="white" className="font-inter" textAnchor="middle" dominantBaseline="middle" style={{fontSize: 'calc(5vw + 1em)'}}>trAIdmark</text>
            <text fill="white" className="font-inter" textAnchor="middle" dominantBaseline="middle" dy="2em" style={{fontSize: 'calc(1vw + 0.5em)'}}>An AI Value Creation Agency</text>
          </svg>
        </div>
    =======
        <div className="flex items-center justify-center w-full h-full">
          <svg ref={logoRef} className="logo z-10" viewBox="0 -175 200 400" style={{margin: 'auto'}}>
            <text fill="white" className="font-inter" textAnchor="middle" dominantBaseline="middle" style={{fontSize: 'calc(5vw + 1em)'}}>trAIdmark</text>
            <text fill="white" className="font-inter" textAnchor="middle" dominantBaseline="middle" dy="2em" style={{fontSize: 'calc(1vw + 0.5em)'}}>An AI Value Creation Agency</text>
          </svg>
        </div>
    >>>>>>> updated
    ```
    
    This change adds `margin: 'auto'` to the SVG element, which will center it within its flex container along both the x and y axes.

--- components/SupernovaLogo/SupernovaLogo.tsx
+++ components/SupernovaLogo/SupernovaLogo.tsx
@@ -9,7 +9,7 @@ const SupernovaLogo = () => {
 
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <svg ref={logoRef} className="logo z-10" viewBox="0 -175 200 400">
+      <svg ref={logoRef} className="logo z-10" viewBox="0 -175 200 400" style={{margin: 'auto'}}>
         <text fill="white" className="font-inter" textAnchor="middle" dominantBaseline="middle" style={{fontSize: 'calc(5vw + 1em)'}}>trAIdmark</text>
         <text fill="white" className="font-inter" textAnchor="middle" dominantBaseline="middle" dy="2em" style={{fontSize: 'calc(1vw + 0.5em)'}}>An AI Value Creation Agency</text>
       </svg>
