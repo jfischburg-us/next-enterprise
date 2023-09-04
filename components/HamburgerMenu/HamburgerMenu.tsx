5fe7d642b8ffe8b2591a3f5f6c6843b30f70b380
Joshua Fischburg <jfischburg@lifemesh.ai>
2023-09-04 15:36:06 -0400

    aider: Added activeMenu state to keep track of the currently active main menu item.
    # Aider chat conversation:
    
    USER: This generated an Unhandled Runtime error: activeMenu is not defined.
    
    ASSISTANT: It seems like the `activeMenu` state is not defined in the `HamburgerMenu.tsx` file. We need to add it to keep track of the currently active main menu item.
    
    Here is the change we need to make:
    
    ```tsx
    components/HamburgerMenu/HamburgerMenu.tsx
    <<<<<<< HEAD
    const HamburgerMenu = () => {
      const [isOpen, setIsOpen] = useState(false);
    =======
    const HamburgerMenu = () => {
      const [isOpen, setIsOpen] = useState(false);
      const [activeMenu, setActiveMenu] = useState(null);
    >>>>>>> updated
    ```
    
    This change adds a new state variable `activeMenu` and its setter function `setActiveMenu` using the `useState` hook from React. Initially, `activeMenu` is set to `null`, indicating that no main menu item is active. When a main menu item is clicked, `setActiveMenu` will be called with the name of the clicked item, setting it as the active menu.

--- components/HamburgerMenu/HamburgerMenu.tsx
+++ components/HamburgerMenu/HamburgerMenu.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [activeMenu, setActiveMenu] = useState(null);
 
   useEffect(() => {
     const handleEsc = (event) => {
