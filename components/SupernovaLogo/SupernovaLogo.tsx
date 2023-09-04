e2ce02007af41104cd844e4788ae48aa0f550204
Joshua Fischburg <jfischburg@lifemesh.ai>
2023-09-04 14:09:24 -0400

    Change the viewBox and font size of the SupernovaLogo component.

--- components/SupernovaLogo/SupernovaLogo.tsx
+++ components/SupernovaLogo/SupernovaLogo.tsx
@@ -8,8 +8,8 @@ const SupernovaLogo = () => {
   }, []);
 
   return (
-    <svg ref={logoRef} className="logo w-full h-full flex items-center justify-center" viewBox="0 0 200 200">
-      <text fill="white" className="font-inter text-9xl" textAnchor="middle" dominantBaseline="middle">trAIdmark</text>
+    <svg ref={logoRef} className="logo w-full h-full flex items-center justify-center" viewBox="0 -50 100 300">
+      <text fill="white" className="font-inter" textAnchor="middle" dominantBaseline="middle">trAIdmark</text>
     </svg>
   );
 };
