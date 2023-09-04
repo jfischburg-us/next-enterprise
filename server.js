bd2a1a2130a864f9c90285ebebcbe703c3d8cd2a
Joshua Fischburg <jfischburg@lifemesh.ai>
2023-09-04 18:13:32 -0400

    Updated server.js to correct import order.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+import next from 'next';
 import { createServer } from 'http';
 import { parse } from 'url';
-import next from 'next';
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
