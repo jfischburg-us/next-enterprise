bde6783e5c0de1241ff9a80618f02371beabcebe
Joshua Fischburg <jfischburg@lifemesh.ai>
2023-09-05 08:30:02 -0400

    Cleaning up repo.

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -32,15 +32,17 @@ const button = cva(
   }
 )
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLAnchorElement>, VariantProps<typeof button> {
-  underline?: boolean
-  href: string
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLAnchorElement>,
+    VariantProps<typeof button> {
+  underline?: boolean;
+  href: string;
 }
 
-export function Button({ className, intent, size, underline, ...props }: ButtonProps) {
-  return (
-    <a className={twMerge(button({ intent, size, className, underline }))} {...props}>
-      {props.children}
-    </a>
-  )
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLAnchorElement>,
+    VariantProps<typeof button> {
+  underline?: boolean;
+  href: string;
 }
+
