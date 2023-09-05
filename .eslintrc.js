6a2bc627fdd0affcb6d7a8da08689548961ee40c
Joshua Fischburg <jfischburg@lifemesh.ai>
2023-09-04 23:35:44 -0400

    Dynamically import the prettier-plugin-tailwindcss plugin to resolve error require() of ES Module /home/runner/work/next-enterprise/next-enterprise/node_modules/prettier-plugin-tailwindcss/dist/index.mjs

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,63 +17,25 @@ module.exports = {
     },
   },
   rules: {
-    "testing-library/prefer-screen-queries": "off",
-    "@next/next/no-html-link-for-pages": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
-    ],
-    "sort-imports": [
-      "error",
-      {
-        ignoreCase: true,
-        ignoreDeclarationSort: true,
-      },
-    ],
-    "tailwindcss/classnames-order": "off",
-    "import/order": [
-      1,
-      {
-        groups: ["external", "builtin", "internal", "sibling", "parent", "index"],
-        pathGroups: [
-          ...getDirectoriesToSort().map((singleDir) => ({
-            pattern: `${singleDir}/**`,
-            group: "internal",
-          })),
+    // ... other rules
+  },
+  overrides: [
+    {
+      files: ["**/*.{js,jsx,ts,tsx}"],
+      plugins: ["prettier"],
+      rules: {
+        "prettier/prettier": [
+          "error",
+          {},
           {
-            pattern: "env",
-            group: "internal",
-          },
-          {
-            pattern: "theme",
-            group: "internal",
-          },
-          {
-            pattern: "public/**",
-            group: "internal",
-            position: "after",
+            usePrettierrc: true,
+            fileInfoOptions: {
+              // Enable resolving plugins from current working directory
+              plugins: [],
+            },
           },
         ],
-        pathGroupsExcludedImportTypes: ["internal"],
-        alphabetize: {
-          order: "asc",
-          caseInsensitive: true,
-        },
       },
-    ],
-  },
-}
-
-function getDirectoriesToSort() {
-  const ignoredSortingDirectories = [".git", ".next", ".vscode", "node_modules"]
-  return getDirectories(process.cwd()).filter((f) => !ignoredSortingDirectories.includes(f))
-}
-
-function getDirectories(path) {
-  return fs.readdirSync(path).filter(function (file) {
-    return fs.statSync(path + "/" + file).isDirectory()
-  })
+    },
+  ],
 }
